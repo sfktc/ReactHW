61b2429b811d2a799630c2b170444b133fb846fe
Oleg <tsoyoleg99@mail.ru>
2023-09-20 14:28:26 +0500

    Правки

--- my-app/src/MyComponent.jsx
+++ my-app/src/MyComponent.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react';
 import styles from './MyComponent.module.css';
 
 export const MyComponent = () => {
+	const [showGreenText, setShowGreenText] = useState(false);
 	const [input, setInput] = useState('');
 	const calcBtns = [];
-	[7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'].forEach((item) => {
+	[7, 8, 9, 4, 5, 6, 1, 2, 3, 0].forEach((item) => {
 		calcBtns.push(
 			<button
 				onClick={(e) => {
@@ -23,26 +24,62 @@ export const MyComponent = () => {
 	return (
 		<div className={styles.wrapper}>
 			{' '}
-			<div className={styles.show_input}>{input}</div>
+			<div
+				className={`${styles.show_input} ${
+					showGreenText ? styles.green : styles.white
+				}`}
+			>
+				{input}
+			</div>
 			<div className={`${styles.digits} ${styles.flex}`}>{calcBtns}</div>
 			<div className={`${styles.modifiers} ${styles.subgrid}`}>
 				{/* delete button */}
-				<button onClick={() => setInput(input.substr(0, input.length - 1))}>
+				<button
+					onClick={() => {
+						if (showGreenText) {
+							setShowGreenText(!showGreenText);
+						}
+						setInput(input.substr(0, input.length - 1));
+					}}
+				>
 					DEL
 				</button>
 				{/* clear all */}
-				<button onClick={() => setInput('')} value="">
-					AC
+				<button
+					onClick={() => {
+						if (showGreenText) {
+							setShowGreenText(!showGreenText);
+						}
+						setInput('');
+					}}
+				>
+					C
 				</button>
 			</div>
 			<div className={`${styles.operations} ${styles.subgrid}`}>
 				{/* plus button */}
-				<button onClick={(e) => setInput(input + e.target.value)} value="+">
+				<button
+					onClick={(e) => {
+						if (showGreenText) {
+							setShowGreenText(!showGreenText);
+						}
+						setInput(input + e.target.value);
+					}}
+					value={'+'}
+				>
 					+
 				</button>
 
 				{/* minus btn */}
-				<button onClick={(e) => setInput(input + e.target.value)} value="-">
+				<button
+					onClick={(e) => {
+						if (showGreenText) {
+							setShowGreenText(!showGreenText);
+						}
+						setInput(input + e.target.value);
+					}}
+					value={'-'}
+				>
 					{' '}
 					-{' '}
 				</button>
@@ -50,12 +87,10 @@ export const MyComponent = () => {
 				<button
 					onClick={(e) => {
 						try {
-							setInput(
-								String(eval(input)).length > 3 &&
-									String(eval(input)).includes('.')
-									? String(eval(input).toFixed(4))
-									: String(eval(input)),
-							);
+							if (!showGreenText) {
+								setShowGreenText(!showGreenText);
+							}
+							setInput(String(eval(input)));
 						} catch (e) {
 							console.log(e);
 						}
